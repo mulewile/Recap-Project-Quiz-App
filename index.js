b4d8affede4d44aec836204c2c4e016deb8fdb9a
William Mulenga Chanda <william.chanda@posteo.de>
2023-03-05 18:12:07 +0100

    added interactivity

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ const markbutton = document.querySelector('[data-js="mark--button"]');
 markbutton.addEventListener("click", () => {
   markbutton.classList.add("marked--dark");
 });
+
+const mainElement = document.querySelector('[data-js="main"]');
+const questionElement = document.querySelector('[data-js="questions"]');
+markbutton.addEventListener("click", () => {
+  questionElement.classList.add("marked--light");
+});
+
+const markButtonOne = document.querySelector('[data-js="markbutton--one"]');
+markButtonOne.addEventListener("click", () => {
+  markButtonOne.classList.add("marked--dark");
+});
+
+const questionOneElement = document.querySelector('[data-js="questions--one"]');
+markButtonOne.addEventListener("click", () => {
+  questionOneElement.classList.add("marked--light");
+});
